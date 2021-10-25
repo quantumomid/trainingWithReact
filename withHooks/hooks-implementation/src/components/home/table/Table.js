95f7ff3636bd47db75d19971260e803b271e108a
quantumomid <omid.wakili@outlook.com>
2021-10-25 19:30:49 +0100

    added HOC component to generate Table

--- withHooks/hooks-implementation/src/components/home/table/Table.js
+++ withHooks/hooks-implementation/src/components/home/table/Table.js
@@ -2,12 +2,29 @@ import TableHeader from './TableHeader.js'
 import TableBody from './TableBody.js'
 import './Table.css';
 
+// // BEFORE
+// export default function Table(){
+//     return(
+//         <table>
+//             <TableHeader/>
+//             <TableBody />
+//         </table>
+//     )
+// }
 
-export default function Table(){
-    return(
+
+
+// // AFTER
+function TableHOC(HeaderHOCComponent, BodyHOCComponent){
+    return function (){
+        return(
         <table>
             <TableHeader/>
             <TableBody />
         </table>
-    )
+        )
+    }
 }
+
+const Table = TableHOC(TableHeader, TableBody)
+export default Table
